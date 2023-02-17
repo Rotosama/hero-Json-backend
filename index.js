90fe77eceee140f3a314ef6baa31cf2bf174af41
Rotosama <n.ruizgarcia@hotmail.com>
2023-02-17 08:37:24 +0100

    formating code

--- index.js
+++ index.js
@@ -12,17 +12,14 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  });
-  
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 // Routes to handle
 app.use("/heroes", heroRoute);
 
 app.listen(PORT, (error) => {
-    if (!error)
-        console.log(`Server up and listening on port: ${PORT}`);
-    else
-        console.log("Cannot start server...", error);
+  if (!error) console.log(`Server up and listening on port: ${PORT}`);
+  else console.log("Cannot start server...", error);
 });
